7b730201dd34cb2b46fd082909787bb310c7dcf5
SlavkoVen <46169342+SlavkoVen@users.noreply.github.com>
2024-09-12 17:34:18 +0300

    Update https_trigger.py

--- https_trigger/https_trigger.py
+++ https_trigger/https_trigger.py
@@ -1,15 +1,16 @@
-# https_trigger.py
 from flask import Flask, request
+import logging
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def https_trigger():
-    print("HTTPS Trigger activated!")
-    print("Request Method:", request.method)
-    print("Request Headers:", request.headers)
-    print("Request Data:", request.data)
-    return "HTTPS Trigger received!", 200
+@app.route('/', methods=['POST'])
+def handle_https_request():
+    # Print request details
+    logging.info('Received HTTPS request')
+    logging.info(f'Headers: {request.headers}')
+    logging.info(f'Body: {request.get_data(as_text=True)}')
+    
+    return 'Request received', 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
