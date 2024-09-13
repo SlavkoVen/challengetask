977f365e77bda914f5fd66a09dfa79539bb3d56d
SlavkoVen <46169342+SlavkoVen@users.noreply.github.com>
2024-09-13 12:20:14 +0300

    Update firestore_trigger.py

--- firestore_trigger/firestore_trigger.py
+++ firestore_trigger/firestore_trigger.py
@@ -1,12 +1,12 @@
-import functions_framework
+from flask import Flask, request
 
-@functions_framework.cloud_event
-def firestore_trigger(cloud_event):
-    # Print event details
-    print(f"Event ID: {cloud_event['id']}")
-    print(f"Event Type: {cloud_event['type']}")
-    print(f"Event Data: {cloud_event['data']}")
+app = Flask(__name__)
 
-    return 'Event received', 200
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+@app.route("/", methods=["POST"])
+def handle_firestore_event():
+    firestore_event = request.json
+    print("Firestore Event Info:", firestore_event)
+    return "Received Firestore event!", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
