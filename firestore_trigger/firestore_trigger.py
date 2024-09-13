ed4fbb86b775117da6f9674f821d467085c1d827
SlavkoVen <46169342+SlavkoVen@users.noreply.github.com>
2024-09-13 13:01:05 +0300

    Update firestore_trigger.py

--- firestore_trigger/firestore_trigger.py
+++ firestore_trigger/firestore_trigger.py
@@ -1,12 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def handle_firestore_event():
-    firestore_event = request.json
-    print("Firestore Event Info:", firestore_event)
-    return "Received Firestore event!", 200
+@app.route('/', methods=['POST'])
+def firestore_trigger():
+    event_data = request.get_json()  # Get the JSON payload sent by Firestore
+    if event_data:
+        # Extract and print event details
+        event_id = event_data.get('id', 'N/A')
+        event_type = event_data.get('type', 'N/A')
+        event_data = event_data.get('data', 'N/A')
+        
+        print(f"Event ID: {event_id}")
+        print(f"Event Type: {event_type}")
+        print(f"Event Data: {event_data}")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+        return jsonify({"message": "Event received"}), 200
+    else:
+        return 'Invalid event data', 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
