9a4bab2ff3d94e9a6525c5107268c8147b93dba4
SlavkoVen <46169342+SlavkoVen@users.noreply.github.com>
2024-09-13 10:50:26 +0300

    Update firestore_trigger.py

--- firestore_trigger/firestore_trigger.py
+++ firestore_trigger/firestore_trigger.py
@@ -8,3 +8,6 @@ def firestore_trigger(cloud_event):
     print(f"Event Data: {cloud_event['data']}")
 
     return 'Event received', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
