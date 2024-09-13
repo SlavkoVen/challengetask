04069f3f093e14df7aca87921880849ba2387109
SlavkoVen <46169342+SlavkoVen@users.noreply.github.com>
2024-09-13 12:37:10 +0300

    Update https_trigger.py

--- https_trigger/https_trigger.py
+++ https_trigger/https_trigger.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
-import logging
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
-def handle_https_request():
-    # Print request details
-    logging.info('Received HTTPS request')
-    logging.info(f'Headers: {request.headers}')
-    logging.info(f'Body: {request.get_data(as_text=True)}')
-    
-    return 'Request received', 200
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def handle_request():
+    if request.method == 'GET':
+        return 'GET request received'
+    elif request.method == 'POST':
+        return 'POST request received'
+    elif request.method == 'PUT':
+        return 'PUT request received'
+    elif request.method == 'DELETE':
+        return 'DELETE request received'
+    else:
+        return 'Method Not Allowed', 405
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
